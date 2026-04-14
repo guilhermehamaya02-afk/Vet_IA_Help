4a6d31785b22a0f81562cf3ed2f43fd4dd515a00
guilhermehamaya02-afk <guilhermehamaya02@gmail.com>
2026-04-13 22:35:20 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 Mensagem_Inicial = print("Olá, eu sou a IA helpvet, estou aqui para auxliar em diagnósticos, responda as perguntas para começarmos.")
 Mensagem_Inicial = print("Ps: Não fui criada para definir diagnóstico e sim para auxiliar.Fica a critério do clínoco o diagnóstico")
 
-Nome = input("Nome do paciente - ")
-Idade = int( input("Idade - ") )
-Peso = float( input("Peso - ") )
-Dor = input("Paciente sente dor? - ")
-Comendo = input("Paciente está se alimentando? - ")
-Sintomas = input("Sintomas do paciente - ")
+Nome = st.text_input("Nome do paciente - ")
+Idade = int( st.number_input("Idade - ") )
+Peso = float( st.number_input("Peso - ") )
+Dor = st.text_input("Paciente sente dor? - ")
+Comendo = st.text_input("Paciente está se alimentando? - ")
+Sintomas = st.text_input("Sintomas do paciente - ")
 
 print(Nome)
 print(type(Idade))
@@ -17,13 +17,13 @@ print(Sintomas)
 
 Realizar_Diagnóstico = print("Paciente apresentando sangue em vômito (Hematêmese) e fraqueza, fortes indícios de Hemorragia interna/Intoxicação; Gastrite ou Úlcera GRAVE; Parvoviróse. Paciente se encontra com mais algum sintoma?")
 
-Duvida = input("Quais?")
+Duvida = st.text_input("Quais?")
 
 print(Duvida)
 
 Conclusão = print("Fortes indícios de Gastríte/Úlcera hemorrágica. Recomenda-se investigar se: Gengíva estiver pálida; Vômitos em repetição. Deixar paciente em observação com reposição de soro fisiológico, realizar exames de sangue e fezes + ultrassom ")
 
-Dúvida_Vet= input("Algo mais?")
+Dúvida_Vet= st.text_input("Algo mais?")
 
 print(Dúvida_Vet)
 
